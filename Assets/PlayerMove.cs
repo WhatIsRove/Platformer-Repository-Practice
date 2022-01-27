561aa4c40e770f5396b3c1415c7c9e3c2f153b47
WhatIsRove <50421316+WhatIsRove@users.noreply.github.com>
2022-01-27 17:14:37 +0000

    Smoother Movement

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -6,18 +6,32 @@ public class PlayerMove : MonoBehaviour
 {
     [SerializeField] public float moveSpeed = 5f;
 
-    [SerializeField] public Rigidbody2D rb;
+    [SerializeField] private float movementSmoothening = 0.5f;
 
-    float horizontalMove;
+    private float horizontalMove;
 
-    // Update is called once per frame
-    void Update()
+    private Rigidbody2D rb;
+
+    private Vector3 velocity = Vector3.zero;
+
+    private void Awake()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal");
+        rb = GetComponent<Rigidbody2D>(); //when it wakes / loads grabs the rigidbody2d of the object the script is attached to.
     }
 
-    void FixedUpdate()
+    // Update is called once per frame
+    private void Update()
     {
-        transform.position += new Vector3(horizontalMove, 0, 0) * moveSpeed * Time.fixedDeltaTime;
+        horizontalMove = Input.GetAxisRaw("Horizontal"); // just gets when ever A or D is pressed and returns -1 to 1, unity has preset buttons like "Fire1" or "Horizontal" / "Vertical"
+    }
+
+    private void FixedUpdate()
+    {
+        //transform.position += new Vector3(horizontalMove, 0, 0) * moveSpeed * Time.fixedDeltaTime; <-- this is a basic move them toward that direction as a Vector3 (x, y, z)
+        
+        Vector3 targetVelocity = new Vector2(horizontalMove * moveSpeed * Time.fixedDeltaTime * 10f, rb.velocity.y); //same thing as above but separates it into velocity to look toward
+
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, movementSmoothening); //and then slowly smoothening it toward that target velocity from your current
+
     }
 }
