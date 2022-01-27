3deecc331f19d6737b8d0c93fb4c998d226c6fa2
WhatIsRove <50421316+WhatIsRove@users.noreply.github.com>
2022-01-27 17:51:00 +0000

    Jumpy Boi is now grounded in reality

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -6,15 +6,16 @@ public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private float jumpVelocity = 40f;
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private LayerMask lmGround;
 
     [Range(0f, 0.3f)][SerializeField] private float movementSmoothening = 0.3f;
 
     private float horizontalMove;
-    private bool jump = false;
 
     private Rigidbody2D rb;
 
     private Vector3 velocity = Vector3.zero;
+    private Vector2 groundedCheck;
 
     private void Awake()
     {
@@ -25,24 +26,22 @@ public class PlayerMove : MonoBehaviour
     private void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal"); // just gets when ever A or D is pressed and returns -1 to 1, unity has preset buttons like "Fire1" or "Horizontal" / "Vertical"
-        if (Input.GetButtonDown("Jump"))
+
+        groundedCheck = (Vector2)transform.position + new Vector2(0, -0.1f);
+        bool bGrounded = Physics2D.OverlapBox(groundedCheck, transform.localScale, 0, lmGround);
+        if (bGrounded && Input.GetButtonDown("Jump"))
         {
-            jump = true;
+            bGrounded = false;
+            rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
         }
     }
 
     private void FixedUpdate()
     {
         //transform.position += new Vector3(horizontalMove, 0, 0) * moveSpeed * Time.fixedDeltaTime; <-- this is a basic move them toward that direction as a Vector3 (x, y, z)
-        
         Vector3 targetVelocity = new Vector2(horizontalMove * moveSpeed * Time.fixedDeltaTime * 10f, rb.velocity.y); //same thing as above but separates it into velocity to look toward
 
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, movementSmoothening); //and then slowly smoothening it toward that target velocity from your current
 
-        if (jump)
-        {
-            jump = false;
-            rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
-        }
     }
 }
