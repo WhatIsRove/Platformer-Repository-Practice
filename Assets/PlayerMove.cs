37ddd20e8d2d6ce9a2e5fdce1070e79f76d9205c
WhatIsRove <50421316+WhatIsRove@users.noreply.github.com>
2022-01-27 17:31:47 +0000

    Oy lordy he jumping (but not grounded)

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] public float moveSpeed = 5f;
+    [SerializeField] private float jumpVelocity = 40f;
+    [SerializeField] private float moveSpeed = 5f;
 
-    [SerializeField] private float movementSmoothening = 0.5f;
+    [Range(0f, 0.3f)][SerializeField] private float movementSmoothening = 0.3f;
 
     private float horizontalMove;
+    private bool jump = false;
 
     private Rigidbody2D rb;
 
@@ -23,6 +25,10 @@ public class PlayerMove : MonoBehaviour
     private void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal"); // just gets when ever A or D is pressed and returns -1 to 1, unity has preset buttons like "Fire1" or "Horizontal" / "Vertical"
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
     }
 
     private void FixedUpdate()
@@ -33,5 +39,10 @@ public class PlayerMove : MonoBehaviour
 
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, movementSmoothening); //and then slowly smoothening it toward that target velocity from your current
 
+        if (jump)
+        {
+            jump = false;
+            rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
+        }
     }
 }
